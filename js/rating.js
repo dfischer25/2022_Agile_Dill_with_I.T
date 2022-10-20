f472b5399a7747e332d17583899046c7f85c0918
dfischer25 <dfischer25@student.cvtc.edu>
2022-10-20 17:41:51 -0500

    [Finishes #183539390] Added pickleball rating page

---
+++ js/rating.js
@@ -0,0 +1,20 @@
+function rate() {
+  var elements = document.getElementsByTagName("input")
+  var total = 0;
+  for (var i = 0; i < elements.length; i++) {
+    if(elements[i].value == "") {
+      alert('Please enter a value between 1 and 5 for all skills');
+      return;
+    } else {
+      let int = parseInt(elements[i].value);
+      if(int < 1 || int > 5) {
+          alert("Please enter a value between 1 and 5 for all skills");
+          return;
+      } else {
+          total += int;
+      }
+    }
+  }
+  total = total / 7;
+  document.getElementById("level").innerHTML = "Your pickleball rating is " + total.toFixed(2);
+}
